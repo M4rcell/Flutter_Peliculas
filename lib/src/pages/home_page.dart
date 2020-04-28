69a3fa73a8e63ba809ac4d108d784e06cb36a319
Marcel <halanocca21@gmail.com>
2020-04-28 17:27:52 -0500

    start use packages flutter_awiper..

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,14 +2,56 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:SafeArea(
-        child: Text('Hola Mundo ...'),)
+      appBar:AppBar(
+        centerTitle: false,
+        title:Text('Peliculas en cine'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: (){}),
+        ],
+
+      ),
+      body:Container(
+        child: Column(
+          children: <Widget>[
+            //Widget Personalisados
+            _swiperTarjetas(),
+          ],
+        ),
+ 
+      ),
       
     );
   }
+
+   Widget _swiperTarjetas(){
+
+     return Container(
+       padding:EdgeInsets.only(top:10.0),
+       //dimenciones de swiper
+       width: double.infinity,
+       height: 300.0,
+       child:new Swiper(
+          layout: SwiperLayout.STACK,
+          itemWidth: 200.0,
+          //itemHeight: 100.0,
+          itemBuilder: (BuildContext context,int index){
+            return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          //pagination: new SwiperPagination(),
+         // control: new SwiperControl(),
+        ),
+     );
+   }
 }
